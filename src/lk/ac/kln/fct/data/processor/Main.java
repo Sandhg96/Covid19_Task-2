d0f9c505121faf617192ed38b80a3c98d2102403
Sandhg96 <hgss1996810@gmai.com>
2020-05-27 23:22:19 +0530

    final

--- src/lk/ac/kln/fct/data/processor/Main.java
+++ src/lk/ac/kln/fct/data/processor/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import lk.ac.kln.fct.data.pojo.Record;
 
 public class Main {
-	private static final String FILE_PATH = "C:/Users/Sandun/eclipse-workspace/Task#2/src/covid-19-data.json";
+	private static final String FILE_PATH = "C:/Users/Sandun/eclipse-workspace/Task#2/Covid19_Task-2/src/covid-19-data.json";
 	private static final String DATE = "18/05/2020";
 	private static final int LIMIT = 1000;
 	
@@ -19,10 +19,9 @@ public class Main {
 	public static void sortAndPrint(ArrayList<Record> records) {
 		records.sort((record1, record2)-> Integer.compare(record2.getCases(), record1.getCases()));
 		
-		for(int i = 0; i < records.size(); i++) {
-			if(records.get(i).getCases() >= LIMIT) {
-				System.out.println(records.get(i).getCountriesAndTerritories() 
-						+ " : " + records.get(i).getCases() + " : " + records.get(i).getDeaths());
+		for(Record r: records) {
+			if(r.getCases() >= LIMIT) {
+				System.out.println(r.getCountriesAndTerritories() + " : " + r.getCases() + " : " + r.getDeaths());
 			}
 		}
 	}
